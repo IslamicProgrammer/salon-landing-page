c008037f54675fa2efa90347da52312cc2be18e5
Murodjon Azamov <63091703+IslamicProgrammer@users.noreply.github.com>
2021-01-26 13:13:08 +0500

    add icons to form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,6 +2,11 @@ import React from "react";
 // images
 import frame from "../../assets/Contact/Frame.png";
 
+import man from "../../assets/Contact/man.svg";
+import mail from "../../assets/Contact/mail.svg";
+import phone from "../../assets/Contact/phone.svg";
+import type from "../../assets/Contact/type.svg";
+
 const Contact = () => {
   return (
     <div className="contact" id="contact">
@@ -31,16 +36,20 @@ const Contact = () => {
         </p>
         <form>
           <div className="input">
+            <img src={man} alt="man" />
             <input type="text" placeholder="Your Name" />
           </div>
           <div className="input">
+            <img src={mail} alt="mail" />
             <input type="email" placeholder="Your Email" />
           </div>
           <div className="input">
+            <img src={phone} alt="phone" />
             <input type="number" placeholder="Your Phone" />
           </div>
           <div className="input">
-            <textarea type="number" placeholder="Your Messasge" />
+            <img className="type" src={type} alt="type" />
+            <textarea type="number" placeholder="Your Messasge"></textarea>
           </div>
           <div className="input">
             <button type="submit" type="number">
